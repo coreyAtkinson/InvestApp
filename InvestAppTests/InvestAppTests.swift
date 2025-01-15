92c2df8085bcc14099ed368f947f74efe9849817
Corey Atkinson <catkinson2025@gmail.com>
2025-01-15 11:13:45 -0600

    Initial Commit

---
+++ InvestAppTests/InvestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InvestAppTests.swift
+//  InvestAppTests
+//
+//  Created by COREY ATKINSON on 1/15/25.
+//
+
+import Testing
+@testable import InvestApp
+
+struct InvestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
